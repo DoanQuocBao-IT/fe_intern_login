224dbf42008b4d0abf1a904d0900cd7137c04c24
Kien Nguyen <Kinnguyen1617@gmail.com>
2023-07-20 08:40:59 +0700

    commit cập nhật giao diện bảng xếp hạng

---
+++ src/screens/Leaderboard.js
@@ -0,0 +1,35 @@
+import React from 'react';
+import { Table } from 'antd';
+
+const Leaderboard = ({ data }) => {
+    const columns = [
+        {
+            title: 'Hạng',
+            dataIndex: 'ranking',
+            key: 'ranking',
+            // fixed: 'left',
+        },
+        {
+            title: 'Thành viên',
+            dataIndex: 'name',
+            key: 'name',
+        },
+        {
+            title: 'Số km',
+            dataIndex: 'distance',
+            key: 'distance',
+        },
+    ];
+
+    return (
+        <div>
+            <Table
+                dataSource={data}
+                columns={columns}
+                scroll={{ x: 'max-content' }}
+            />
+        </div>
+    );
+};
+
+export default Leaderboard;
